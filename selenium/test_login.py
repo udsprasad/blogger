449ac3b77be7869f2444cbf0cf5d270f97dc4dac
udsprasad <64519099+udsprasad@users.noreply.github.com>
2020-09-07 23:18:21 +0530

    updated chrome_options

--- selenium/test_login.py
+++ selenium/test_login.py
@@ -10,7 +10,7 @@ class Test(unittest.TestCase):
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument("window-size=1200x600")
+        chrome_options.add_argument("--window-size=1920,1080")
         self.driver=webdriver.Chrome(executable_path='driver/chromedriver_linux64/chromedriver',options=chrome_options)
 
 
